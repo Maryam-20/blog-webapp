8a53e5a0237f0f197e46d8c818f527bf4f5118ff
Anileleye Maryam Oluwakemi <maryamanileleye@gmail.com>
2024-12-07 20:04:36 +0100

    Added collected staticfiles for render deployment, Added whitenoise to requirements, Added whitenoise middleware to settings

--- multimedia/settings.py
+++ multimedia/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -146,22 +147,21 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'  # URL prefix for accessing static files
+
+# Directory where you keep your static files during development
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_URL = '/static/'
+# Directory where static files are collected during production (this is required by render deployments)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-
-# Directory to collect all static files for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
